59f0b87d3ff770e31b139ea35ddac0d29578ebfd
enrocha0312 <enrocha0312@gmail.com>
2024-04-13 21:27:31 -0300

    comecando projeto

---
+++ src/main/java/br/projetofinalwebservices/eduardondarocha/EduardondarochaApplication.java
@@ -0,0 +1,13 @@
+package br.projetofinalwebservices.eduardondarocha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduardondarochaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduardondarochaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/projetofinalwebservices/eduardondarocha/EduardondarochaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.projetofinalwebservices.eduardondarocha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduardondarochaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
